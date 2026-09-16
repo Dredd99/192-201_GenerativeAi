982b909851624066bda32421cf3c5d0248c9ad50
Kaung Thant Maung Maung <6705140022@siam.edu>
2026-09-17 01:53:42 +0700

    Add Vehicle class for rental management

--- Assignment_02/rental.py
+++ Assignment_02/rental.py
@@ -1,1 +1,16 @@
+class Vehicle:
+    def __init__(self, make, model, plate):
+        self.make = make
+        self.model = model
+        self.plate = plate
+        self.is_rented = False
 
+    def rent(self):
+        self.is_rented = True
+
+    def return_vehicle(self):
+        self.is_rented = False
+
+    def __str__(self):
+        status = "rented" if self.is_rented else "available"
+        return f"{self.make} {self.model} ({self.plate}) [{status}]"
